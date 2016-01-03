5ed948eef92c749f9579f11b5b67e0508fe55970
AAZ H <aazhbd@yahoo.com>
2016-01-03 10:45:34 +0100

    Database character set changed

--- articulatelogic.sql
+++ articulatelogic.sql
@@ -3,8 +3,7 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`articulatelogic_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
+CREATE DATABASE IF NOT EXISTS `articulatelogic_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `articulatelogic_db`;
 
 /*Table structure for table `articles` */
