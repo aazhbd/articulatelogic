7b2b968abf2abb41c41d75899d690b34a4ec481e
AAZ H <aazhbd@yahoo.com>
2016-01-10 18:08:24 +0100

    Production configuration changes

--- conf.php
+++ conf.php
@@ -2,8 +2,21 @@
 
 $conf = array(
     'production' => array(
-        'db_name' => 'prod_db',
-        'development_mode' => false
+        'db_name' => 'articulatelogic_db',
+        'db_host' => 'localhost',
+        'db_user' => 'root',
+        'db_pass' => '',
+        'development_mode' => true,
+        'path_url' => 'http://beta.articulatelogic.com/',
+        'path_root' => '/',
+        'path_sys_template' => '/Template/base.twig',
+        'path_static' => '/Template/static/',
+        'path_user_template' => '/App/views',
+        'user_var' => array(
+            'project_name' => 'ArticulateLogic.com',
+            'project_static' => '/App/static',
+            'files_dir' => '../../file_dir'
+        ),
     ),
     'development' => array(
         'db_name' => 'articulatelogic_db',
