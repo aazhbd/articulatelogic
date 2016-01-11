e01a07882f03c37bd3dc7c87bc4707805a474c48
AAZ H <aazhbd@yahoo.com>
2016-01-11 09:31:29 +0100

    Production configuration changes

--- ArtLibs/Files.php
+++ ArtLibs/Files.php
@@ -197,3 +197,11 @@ class Files
         return $executed;
     }
 }
+
+/**
+ * An open source web application development framework for PHP 5.
+ * @author        ArticulateLogic Labs
+ * @author        Abdullah Al Zakir Hossain, Email: aazhbd@yahoo.com
+ * @copyright     Copyright (c)2009-2014 ArticulateLogic Labs
+ * @license       MIT License
+ */
