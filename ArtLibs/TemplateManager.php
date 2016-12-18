9d67235862d251d54fa14ca6857adc4516e50bad
AAZ H <aazhbd@yahoo.com>
2016-12-18 21:03:26 +0100

    Minor Fix

--- ArtLibs/TemplateManager.php
+++ ArtLibs/TemplateManager.php
@@ -45,6 +45,6 @@ class TemplateManager
  * An open source web application development framework for PHP 5.
  * @author        ArticulateLogic Labs
  * @author        Abdullah Al Zakir Hossain, Email: aazhbd@yahoo.com
- * @copyright     Copyright (c)2009-2014 ArticulateLogic Labs
+ * @copyright     Copyright (c)2009-2016 ArticulateLogic Labs
  * @license       MIT License
  */
